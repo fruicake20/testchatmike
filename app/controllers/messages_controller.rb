5101c8f4aa78e8607d4ea88242348f7f941b0dd0
Michael Agtual <mike_tech@mail.eyp.ph>
2017-07-04 13:00:48 +0800

    fix redirection of pages and session username

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,46 @@
 class MessagesController < ApplicationController
   def index
+
+    if session[:username] != nil
+
     @messages = Message.all
+
+        else
+
+      redirect_to(:action => 'login')
+
+    end
+
   end
 
   def create
+    
   	hello = params[:message]
     hello[:content] =  session[:username]+': '+hello[:content]
 
     @message = Message.create!(hello)
     PrivatePub.publish_to("/messages/new", message: @message)
+
+
   end
 
   def login
-	 session[:username] = params[:username]
-	# redirect_to(:action => 'index')
+
   end
+
+
+  def create_username
+
+
+     if !params[:username].blank?
+
+       redirect_to(:action => 'index')
+       session[:username] = params[:username]
+
+
+     end
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Chatter::Application.routes.draw do
 
 
   match 'login', :to => 'messages#login'
+  match 'create_username', :to => 'messages#create_username'
 
   resources :messages
-  root to: 'messages#index'
+  root to: 'messages#login'
 
  
 
